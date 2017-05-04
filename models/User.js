caf63558f4b95f81e0d754a38f72a4fcfdf21462
Bilal <bilal.ahmed@vectracom.com>
2017-05-04 17:59:31 +0500

    Making login

--- models/User.js
+++ models/User.js
@@ -1,4 +1,32 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+
+
 const UsersSchema = mongoose.Schema({name:{type: String ,require:true},password:{type: String, require:true }})
 
 const User= module.exports=mongoose.model("User",UsersSchema)
+
+module.exports.addUser= function(newUser,callback){
+    bcrypt.genSalt(10,(err,salt)=>{
+        if(err){
+            throw err;           
+        }
+
+        bcrypt.hash(newUser.password,salt,(err,hash)=>{
+                // const newUser2 = new User({
+                //     "name":newUser.name,
+                //     "pasword":hash
+                // });
+                if(err){
+                    throw err;           
+                }
+                newUser.password=hash;
+                newUser.save(callback);
+            })
+    })
+}
+
+module.exports.getUserByName = function(userName,callback){
+    const qurey = {name: userName};
+    User.findOne(qurey,callback);
+}
--- routes/users.js
+++ routes/users.js
@@ -7,14 +7,47 @@ router.get("/mypage",(req,res)=>{
     res.send("Welcome to my router");
 });
 
+router.post("/login",(req,res)=>{
+    console.log("Welcome to my router");
+    const username = req.body.name;
+    const userpass = req.body.password;
+    User.getUserByName(username,(err,user)=>{
+        if(err) throw err;
+        if(!user){
+            res.json({success:false,message:"User Name is not valid"});
+        }else{
+            res.json({success:true,message:"Loged In"});
+        }
+    });
+    
+
+});
+
 router.post("/mydata",(req,res)=>{
     console.log(req.body);
-    res.send("Thanks man");
+    //res.send("Thanks man");
     //const newUser= new User({name: "bilal", password:"123"});
-    const newUser= new User(req.body);
-    newUser.save((err, User)=>{
-        console.log("User added succusfully "+User.name+" "+User.password);
+    const newUser= new User({
+        name:req.body.name,
+       password:req.body.password 
     });
+
+    User.addUser(newUser, (err, user)=>
+    {
+        if(err)
+        {
+            res.json({success:false, msg:"Failed to register user!"});
+        }
+        else
+        {
+            res.json({success:true,msg:"User added"});
+        }
+    });
+    //newUser.save((err, User)=>{
+      //  console.log("User added succusfully "+User.name+" "+User.password);
+   // });
 });
+
+
 
 module.exports= router;
